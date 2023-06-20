a3cd746cfd13111237e300c6b29684e042561388
jjpark58 <68671561+jjpark58@users.noreply.github.com>
2023-06-20 00:46:15 -0700

    signal tap working

--- James_Park_70315379_Lab_4/rtl/lab4/db/ip/sld73f98f60/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ James_Park_70315379_Lab_4/rtl/lab4/db/ip/sld73f98f60/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 9bf7f14905184415dab3
+//   DESIGN_HASH   : 61794304e0b1abe3651c
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h9bf7f14905184415dab3 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h61794304e0b1abe3651c };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
